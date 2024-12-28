88ac4a3ff7bde56f84a1fbc343e09497bd70327c
Gerry Vien Flores <gerryvienlifeflores@gmail.com>
2024-12-28 22:19:14 +0800

    Pagwawasto ng mga salita

--- app/src/main/java/com/example/gca_/InfoFragment.java
+++ app/src/main/java/com/example/gca_/InfoFragment.java
@@ -1,10 +1,47 @@
 package com.example.gca_;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class InfoFragment extends Fragment {
     public InfoFragment() {
         super(R.layout.fragment_info);  // The layout for this fragment
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageButton pkr = view.findViewById(R.id.imageButton3);
+        ImageButton jsa = view.findViewById(R.id.imageButton7);
+        ImageButton ins = view.findViewById(R.id.imageButton8);
+
+        TextView name = view.findViewById(R.id.nameText);
+        TextView title = view.findViewById(R.id.person_title);
+
+        pkr.setOnClickListener(v -> {
+            name.setText("Princess Key R. Reniva".toUpperCase());
+            title.setText("Mananaliksik");
+
+        });
+
+        jsa.setOnClickListener(v -> {
+            name.setText("Joanabell C. San Agustin".toUpperCase());
+            title.setText("Mananaliksik");
+
+        });
+
+        ins.setOnClickListener(v -> {
+            name.setText("Edwin R. Ichiano".toUpperCase() + ", PhD");
+            title.setText("Tagapayo");
+
+        });
     }
 }
--- app/src/main/java/com/example/gca_/ManualFragment.java
+++ app/src/main/java/com/example/gca_/ManualFragment.java
@@ -1,10 +1,36 @@
 package com.example.gca_;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 
 public class ManualFragment extends Fragment {
     public ManualFragment() {
         super(R.layout.fragment_manual);  // The layout for this fragment
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button intindi = view.findViewById(R.id.agreeButton);
+        intindi.setOnClickListener(v -> {
+            navigateToFragment(new FrontPage());
+        });
+    }
+
+    private void navigateToFragment(Fragment fragment) {
+        if (getActivity() != null) {
+            // Replace the current fragment and add the transaction to the back stack
+            getActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .remove(this) // This ensures you can go back
+                    .commit();
+        }
     }
 }
